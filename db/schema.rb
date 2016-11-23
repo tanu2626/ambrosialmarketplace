9a3fd352633360fb7e08e1295c852682a6c0d240
Tanu <tanuuu.21@gmail.com>
2016-11-23 20:20:47 +1100

    added products scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123052146) do
+ActiveRecord::Schema.define(version: 20161123091114) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price"
+    t.boolean  "availability"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
