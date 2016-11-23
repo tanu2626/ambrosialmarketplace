0c4b4e12008faed97225de36359ba30c12b8fee9
Tanu <tanuuu.21@gmail.com>
2016-11-23 21:41:55 +1100

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
+
   def home
     if current_user
         redirect_to products_path
     end
+  end
+
+
+  def dashboard
+     @products = current_user.products
   end
 end
